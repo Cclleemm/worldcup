e9d9bd543da6f9e2246c25725c7defae57708cc9
Clément Raussin <clement@raussin.com>
2015-10-06 10:08:43 +0200

    Update Bet.php

--- app/models/Bet.php
+++ app/models/Bet.php
@@ -83,47 +83,47 @@ class Bet extends Eloquent {
 
     public function inDistance($points){
         switch ($this->distance_points) {
-            case BET::$DISTANCE_0_TO_5:
+            case Bet::$DISTANCE_0_TO_5:
                 return ($points >= 0 && $points < 5);
-            case BET::$DISTANCE_5_TO_10:
+            case Bet::$DISTANCE_5_TO_10:
                 return ($points >= 5 && $points < 10);
-            case BET::$DISTANCE_10_TO_15:
+            case Bet::$DISTANCE_10_TO_15:
                 return ($points >= 10 && $points < 15);
-            case BET::$DISTANCE_15_TO_20:
+            case Bet::$DISTANCE_15_TO_20:
                 return ($points >= 15 && $points < 20);
-            case BET::$DISTANCE_20_TO_25:
+            case Bet::$DISTANCE_20_TO_25:
                 return ($points >= 20 && $points < 25);
-            case BET::$DISTANCE_25_TO_30:
+            case Bet::$DISTANCE_25_TO_30:
                 return ($points >= 25 && $points < 30);
-            case BET::$DISTANCE_30_TO_35:
+            case Bet::$DISTANCE_30_TO_35:
                 return ($points >= 30 && $points < 35);
-            case BET::$DISTANCE_35_TO_40:
+            case Bet::$DISTANCE_35_TO_40:
                 return ($points >= 35 && $points < 40);
-            case BET::$DISTANCE_40_TO_45:
+            case Bet::$DISTANCE_40_TO_45:
                 return ($points >= 40 && $points < 45);
-            case BET::$DISTANCE_45_TO_50:
+            case Bet::$DISTANCE_45_TO_50:
                 return ($points >= 45 && $points < 50);
-            case BET::$DISTANCE_50_TO_55:
+            case Bet::$DISTANCE_50_TO_55:
                 return ($points >= 50 && $points < 55);
-            case BET::$DISTANCE_55_TO_60:
+            case Bet::$DISTANCE_55_TO_60:
                 return ($points >= 55 && $points < 60);
-            case BET::$DISTANCE_60_TO_65:
+            case Bet::$DISTANCE_60_TO_65:
                 return ($points >= 60 && $points < 65);
-            case BET::$DISTANCE_65_TO_70:
+            case Bet::$DISTANCE_65_TO_70:
                 return ($points >= 65 && $points < 70);
-            case BET::$DISTANCE_70_TO_75:
+            case Bet::$DISTANCE_70_TO_75:
                 return ($points >= 70 && $points < 75);
-            case BET::$DISTANCE_75_TO_80:
+            case Bet::$DISTANCE_75_TO_80:
                 return ($points >= 75 && $points < 80);
-            case BET::$DISTANCE_80_TO_85:
+            case Bet::$DISTANCE_80_TO_85:
                 return ($points >= 80 && $points < 85);
-            case BET::$DISTANCE_85_TO_90:
+            case Bet::$DISTANCE_85_TO_90:
                 return ($points >= 85 && $points < 90);
-            case BET::$DISTANCE_90_TO_95:
+            case Bet::$DISTANCE_90_TO_95:
                 return ($points >= 90 && $points < 95);
-            case BET::$DISTANCE_95_TO_100:
+            case Bet::$DISTANCE_95_TO_100:
                 return ($points >= 95 && $points < 100);
-            case BET::$DISTANCE_100_PLUS:
+            case Bet::$DISTANCE_100_PLUS:
                 return ($points >= 100);
         }
     }
@@ -148,27 +148,27 @@ class Bet extends Eloquent {
      */
     public static function getDistances(){
         return array(
-            array('id' => BET::$DISTANCE_0_TO_5, 'name' => '0 à 4'),
-            array('id' => BET::$DISTANCE_5_TO_10, 'name' => '5 à 9'),
-            array('id' => BET::$DISTANCE_10_TO_15, 'name' => '10 à 14'),
-            array('id' => BET::$DISTANCE_15_TO_20, 'name' => '15 à 19'),
-            array('id' => BET::$DISTANCE_20_TO_25, 'name' => '20 à 24'),
-            array('id' => BET::$DISTANCE_25_TO_30, 'name' => '25 à 29'),
-            array('id' => BET::$DISTANCE_30_TO_35, 'name' => '30 à 34'),
-            array('id' => BET::$DISTANCE_35_TO_40, 'name' => '35 à 39'),
-            array('id' => BET::$DISTANCE_40_TO_45, 'name' => '40 à 44'),
-            array('id' => BET::$DISTANCE_45_TO_50, 'name' => '45 à 49'),
-            array('id' => BET::$DISTANCE_50_TO_55, 'name' => '50 à 54'),
-            array('id' => BET::$DISTANCE_55_TO_60, 'name' => '55 à 59'),
-            array('id' => BET::$DISTANCE_60_TO_65, 'name' => '60 à 64'),
-            array('id' => BET::$DISTANCE_65_TO_70, 'name' => '65 à 69'),
-            array('id' => BET::$DISTANCE_70_TO_75, 'name' => '70 à 74'),
-            array('id' => BET::$DISTANCE_75_TO_80, 'name' => '75 à 79'),
-            array('id' => BET::$DISTANCE_80_TO_85, 'name' => '80 à 84'),
-            array('id' => BET::$DISTANCE_85_TO_90, 'name' => '85 à 89'),
-            array('id' => BET::$DISTANCE_90_TO_95, 'name' => '90 à 94'),
-            array('id' => BET::$DISTANCE_95_TO_100, 'name' => '95 à 99'),
-            array('id' => BET::$DISTANCE_100_PLUS, 'name' => '100+'),
+            array('id' => Bet::$DISTANCE_0_TO_5, 'name' => '0 à 4'),
+            array('id' => Bet::$DISTANCE_5_TO_10, 'name' => '5 à 9'),
+            array('id' => Bet::$DISTANCE_10_TO_15, 'name' => '10 à 14'),
+            array('id' => Bet::$DISTANCE_15_TO_20, 'name' => '15 à 19'),
+            array('id' => Bet::$DISTANCE_20_TO_25, 'name' => '20 à 24'),
+            array('id' => Bet::$DISTANCE_25_TO_30, 'name' => '25 à 29'),
+            array('id' => Bet::$DISTANCE_30_TO_35, 'name' => '30 à 34'),
+            array('id' => Bet::$DISTANCE_35_TO_40, 'name' => '35 à 39'),
+            array('id' => Bet::$DISTANCE_40_TO_45, 'name' => '40 à 44'),
+            array('id' => Bet::$DISTANCE_45_TO_50, 'name' => '45 à 49'),
+            array('id' => Bet::$DISTANCE_50_TO_55, 'name' => '50 à 54'),
+            array('id' => Bet::$DISTANCE_55_TO_60, 'name' => '55 à 59'),
+            array('id' => Bet::$DISTANCE_60_TO_65, 'name' => '60 à 64'),
+            array('id' => Bet::$DISTANCE_65_TO_70, 'name' => '65 à 69'),
+            array('id' => Bet::$DISTANCE_70_TO_75, 'name' => '70 à 74'),
+            array('id' => Bet::$DISTANCE_75_TO_80, 'name' => '75 à 79'),
+            array('id' => Bet::$DISTANCE_80_TO_85, 'name' => '80 à 84'),
+            array('id' => Bet::$DISTANCE_85_TO_90, 'name' => '85 à 89'),
+            array('id' => Bet::$DISTANCE_90_TO_95, 'name' => '90 à 94'),
+            array('id' => Bet::$DISTANCE_95_TO_100, 'name' => '95 à 99'),
+            array('id' => Bet::$DISTANCE_100_PLUS, 'name' => '100+'),
         );
     }
 }
